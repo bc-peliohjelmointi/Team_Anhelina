b145efd3c1f851eaf83e579aa67eb13b58966c8e
Anhelina <s2401148@edu.bc.fi>
2025-12-12 05:54:24 +0200

    нИчИго

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadScene("vanya");
     }
 
     public void QuitGame()
