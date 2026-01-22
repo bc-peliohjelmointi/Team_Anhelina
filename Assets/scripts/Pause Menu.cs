f87388e962ef1ae5c797710cb39e36eff80ccfc8
s2401224 <s2401224@edu.bc.fi>
2026-01-22 15:01:19 +0200

    esc canvas

---
+++ Assets/scripts/Pause Menu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("ESC pressed");
+            if (isPaused)
+                Play();
+            else
+                Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Play()
+    {
+        Debug.Log("PLAY CLICKED");
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Home()
+    {
+        Time.timeScale = 1f; // ОБЯЗАТЕЛЬНО
+        SceneManager.LoadScene(0);
+    }
+}
