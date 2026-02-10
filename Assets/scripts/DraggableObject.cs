baa34261701ac94d7fee643b3661ab508a86db30
VovaParasochka <s2300896@edu.bc.fi>
2026-02-10 09:28:42 +0200

    Scene 1 | script mechanica

--- Assets/scripts/DraggableObject.cs
+++ Assets/scripts/DraggableObject.cs
@@ -3,10 +3,29 @@ using UnityEngine;
 public class DraggableObject : MonoBehaviour
 {
     public Rigidbody rb;
+    public float pushForce = 5f;
+    public bool canBePushed = true;
 
-    private void Awake()
+    void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+
+        if (rb.mass < 1f)
+        {
+            rb.mass = 1f;
+        }
     }
 
+    public void Push(Vector3 direction, float force)
+    {
+        if (canBePushed && rb != null)
+        {
+            rb.AddForce(direction * force * pushForce, ForceMode.Impulse);
+        }
+    }
 }
