9965466d08885e1e56bb4cf0f50fb1bb55566c48
s2401224 <s2401224@edu.bc.fi>
2026-03-09 15:01:05 +0200

    story 1/2 made

---
+++ Assets/scripts/HoverEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HoverEffect: MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    Outline outline;
+
+    void Start()
+    {
+        outline = GetComponent<Outline>();
+        outline.enabled = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        outline.enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        outline.enabled = false;
+    }
+}
